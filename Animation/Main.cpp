0730c566e2fdaf4d2d797ea04f8acabf8294968d
kaytopod0119 <137128977+kaytopad@users.noreply.github.com>
2026-07-06 15:37:21 +0900

    アニメーションサンプル追加

---
+++ Animation/Main.cpp
@@ -0,0 +1,19 @@
+﻿#include "Dxlib.h"
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+	// Initialize DxLib
+	if (DxLib_Init() == -1) { return -1;}
+	// Main loop
+	while (ProcessMessage() == 0 && CheckHitKey(KEY_INPUT_ESCAPE) == 0) 
+	{
+		// Clear the screen
+		ClearDrawScreen();
+		SetFontSize(30);
+		DrawFormatString(77, 250, GetColor(255, 100, 0), "Just Do It!");
+		// Update the screen
+		ScreenFlip();
+	}
+	// Cleanup and exit
+	DxLib_End();
+	return 0;
+}
